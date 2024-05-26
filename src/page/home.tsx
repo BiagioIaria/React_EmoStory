69814fea96fe9cd5b0305f8ca72cca13e8fcf11f
Biagio mac <biagioiaria21@gmail.com>
2024-05-26 13:04:20 +0200

    test

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,8 +1,28 @@
 import './../App.css';
+import Button from '@mui/material/Button';
 
 
 function Home() {
-    return null;
+
+    return (<Button
+        variant="contained"
+        component="label"
+    >
+        Upload File
+        <input
+            type="file"
+            hidden
+            onChange={(event) => {
+                const file = event.target.files![0];
+                console.log(file);
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    console.log(e.target!.result);
+                };
+                reader.readAsText(file);
+            }}
+        />
+    </Button>);
 }
 
 export default Home;
