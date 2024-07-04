8de1d22e9f562c4eef6dc300c0a9c250453f2ccb
Biagio mac <biagioiaria21@gmail.com>
2024-07-04 11:56:00 +0200

    edit

--- src/page/Edit.tsx
+++ src/page/Edit.tsx
@@ -134,7 +134,7 @@ function rowContent(_index: number, row: Data) {
     );
 }
 
-export default function ReactVirtualizedTable() {
+export default function Edit() {
     return (
         <Paper style={{ height: 400, width: '50%' }}>
             <TableVirtuoso
