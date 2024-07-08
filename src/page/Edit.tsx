966da72dbd56885e03bc1214f459625064227d1e
Biagio mac <biagioiaria21@gmail.com>
2024-07-08 17:44:53 +0200

    edit

--- src/page/Edit.tsx
+++ src/page/Edit.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import TableCell from '@mui/material/TableCell';
-import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { TableVirtuoso } from 'react-virtuoso';
 import Button from '@mui/material/Button';
 
 interface Data {
     [key: string]: string;
-    none: string;
     preUnit: string;
     pre1: string;
     plan1: string;
@@ -38,7 +36,6 @@ const sample: readonly Sample[] = [
 ];
 
 function createData(
-    none: string,
     preUnit: string,
     pre1: string,
     plan1: string,
@@ -49,69 +46,61 @@ function createData(
     eff2: string,
     effUnit: string,
 ): Data {
-    return { none, preUnit, pre1, plan1, eff1, sc, pre2, plan2, eff2, effUnit };
+    return { preUnit, pre1, plan1, eff1, sc, pre2, plan2, eff2, effUnit };
 }
+const columnWidth = 120;
 
 const initialColumns: ColumnData[] = [
     {
-        width: 0,
-        label: '',
-        dataKey: 'none',
-    },
-    {
-        width: 120,
+        width: columnWidth,
         label: 'preUnit',
         dataKey: 'preUnit',
     },
     {
-        width: 120,
+        width: columnWidth,
         label: 'pre1',
         dataKey: 'pre1',
     },
     {
-        width: 120,
+        width: columnWidth,
         label: 'plan1',
         dataKey: 'plan1',
     },
     {
-        width: 120,
+        width: columnWidth,
         label: 'eff1',
         dataKey: 'eff1',
     },
     {
-        width: 120,
+        width: columnWidth,
         label: 'sc',
         dataKey: 'sc',
     },
     {
-        width: 120,
+        width: columnWidth,
         label: 'pre2',
         dataKey: 'pre2',
     },
     {
-        width: 120,
+        width: columnWidth,
         label: 'plan2',
         dataKey: 'plan2',
     },
     {
-        width: 120,
+        width: columnWidth,
         label: 'eff2',
         dataKey: 'eff2',
     },
     {
-        width: 120,
+        width: columnWidth,
         label: 'effUnit',
         dataKey: 'effUnit',
     }
 ];
 
-const nameRow = [
-    'Title', 'Unit', 'Pre-Eff Unit', 'Plan', 'Goal', 'Agent', 'Value1', 'Value2', 'Emo1', 'Emo2'
-];
-
 const initialRows: Data[] = Array.from({ length: sample.length }, (_, index) => {
     const selection = sample[index];
-    return createData(nameRow[index], ...selection);
+    return createData(...selection);
 });
 
 const buttonCells: [number, keyof Data, boolean][] = [
@@ -120,67 +109,24 @@ const buttonCells: [number, keyof Data, boolean][] = [
 ];
 
 function Edit() {
-    const [columns, setColumns] = React.useState<ColumnData[]>(initialColumns);
     const [rows, setRows] = React.useState<Data[]>(initialRows);
 
-    const addColumns = () => {
-        const newColumnKeys = [`newColumn${columns.length}`, `newColumn${columns.length + 1}`];
-        const newColumns = newColumnKeys.map(key => ({
-            width: 120, label: key, dataKey: key as keyof Data
-        }));
-
-        setColumns([...columns, ...newColumns]);
-
-        const updatedRows = rows.map(row => ({
-            ...row,
-            [newColumnKeys[0]]: 'New Value 1',
-            [newColumnKeys[1]]: 'New Value 2',
-        }));
-
-        setRows(updatedRows);
+    const addRow = () => {
+        const newRow: Data = createData(
+            'Frozen yoghurt', '159', '6.0', '24', '4.0', '2', '3', '3', '2'
+        );
+        setRows([...rows, newRow]);
     };
 
-    const fixedHeaderContent = () => {
-        const stickyStyle = {
-            width: 200,
-        };
-
-        return (
-            <TableRow>
-                {columns.map((column, colIndex) => (
-                    <TableCell
-                        key={column.dataKey}
-                        variant="head"
-                        align={'right'}
-                        style={colIndex === 0 ? stickyStyle : {width: column.width }}
-                        sx={{
-                            backgroundColor: 'background.paper',
-                        }}
-                    >
-                        {column.label}
-                    </TableCell>
-                ))}
-            </TableRow>
-        );
-    }
-
     const rowContent = (rowIndex: number, row: Data) => {
-        const stickyStyle = {
-            position: 'sticky',
-            left: 0,
-            background: 'white',
-            boxShadow: '2px 0 5px -2px rgba(0,0,0,0.3)',
-        };
-
-
         return (
             <React.Fragment>
-                {columns.map((column, colIndex) => (
+                {initialColumns.map((column) => (
                     <TableCell
                         key={column.dataKey}
                         align={'right'}
-                        // @ts-ignore
-                        style={colIndex === 0 ? stickyStyle : {}}
+                        style={{ width: column.width }}
+                        sx={{ borderRight: '1px solid rgba(224, 224, 224, 1)' }}
                     >
                         {buttonCells.some(([r, c, f]) => r === rowIndex && c === column.dataKey && f) ? (
                             <Button variant="contained" onClick={() => alert(`Button clicked in row ${rowIndex + 1}, column ${column.dataKey}`)}>
@@ -197,11 +143,10 @@ function Edit() {
 
     return (
         <div className='edit'>
-            <Button variant="contained" onClick={addColumns}>Add Columns</Button>
+            <Button variant="contained" onClick={addRow}>Add rows</Button>
             <Paper style={{ height: 400 }}>
                 <TableVirtuoso
                     data={rows}
-                    fixedHeaderContent={fixedHeaderContent}
                     itemContent={rowContent}
                 />
             </Paper>
